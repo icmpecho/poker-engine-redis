3852fcd99998e708c1cd77c6604e40d3a1605a23
Pinnapong <pinnapong@gmail.com>
2016-09-03 21:59:34 +0700

    Add deck module

---
+++ test/deck.js
@@ -0,0 +1,14 @@
+const { Deck } = require('../lib/deck')
+
+describe('Deck', function () {
+
+  describe('constructor', function () {
+
+    it('initials 52 cards', function () {
+      const deck = new Deck()
+      assert.lengthOf(deck.cards, 52)
+    })
+
+  })
+
+})
