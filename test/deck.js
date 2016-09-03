8e0e0952155bd7d720f8fa7a5a654a4646f0918d
Pinnapong <pinnapong@gmail.com>
2016-09-04 00:15:41 +0700

    Add shuffle method to deck

--- test/deck.js
+++ test/deck.js
@@ -2,13 +2,24 @@ const Deck = require('../lib/deck')
 
 describe('Deck', function () {
 
-  describe('constructor', function () {
+  describe('#constructor', function () {
 
     it('initials 52 cards', function () {
       const deck = new Deck()
-      assert.lengthOf(deck.cards, 52)
+      assert.equal(deck.count, 52)
     })
 
   })
 
+  describe('#shuffle', function () {
+
+    it('change card order', function () {
+      const deck = new Deck()
+      const newDeck = new Deck()
+      assert.deepEqual(deck, newDeck)
+      deck.shuffle()
+      assert.notDeepEqual(deck, newDeck)
+    })
+
+  })
 })
