3a3f8a6a15b6df76a9cfabc9a78889ba5008ab59
Pinnapong <pinnapong@gmail.com>
2016-09-04 01:28:23 +0700

    Add blank lines

--- test/card.js
+++ test/card.js
@@ -3,9 +3,12 @@ const Card = require('../lib/card')
 describe('Card', function () {
 
   describe('toString', function () {
+
     it('returns string representation of the card', function () {
       const card = new Card(Card.Suits.spade, 'A')
       assert.equal(card.toString(), 'A♠')
     })
+
   })
+
 })
