56d0525e4d80dda19c180da7d3502092f041e68f
Pinnapong <pinnapong@gmail.com>
2016-09-03 21:59:23 +0700

    Fix invalid card

--- test/card.js
+++ test/card.js
@@ -5,8 +5,8 @@ describe('Card', function () {
 
   describe('toString', function () {
     it('returns string representation of the card', function () {
-      const card = new Card(Suits.spade, '1')
-      assert.equal(card.toString(), '1-S')
+      const card = new Card(Suits.spade, 'A')
+      assert.equal(card.toString(), 'A-S')
     })
   })
 })
