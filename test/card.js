f30c09b3511f4d20617251c63ff443abda951da8
Pinnapong <pinnapong@gmail.com>
2016-09-04 17:24:46 +0700

    Add toJSON to Card

--- test/card.js
+++ test/card.js
@@ -2,13 +2,20 @@ const Card = require('../lib/card')
 
 describe('Card', function () {
 
-  describe('toString', function () {
+  describe('#toString', function () {
 
     it('returns string representation of the card', function () {
       const card = new Card(Card.Suits.spade, 'A')
       assert.equal(card.toString(), 'A♠')
     })
+  })
 
+  describe('#toJSON', function () {
+
+    it('returns basic string representation of the card', function () {
+      const card = new Card(Card.Suits.spade, 'A')
+      assert.equal(card.toJSON(), 'AS')
+    })
   })
 
 })
