33963458cc62d53b5b86388caee5ebdb24f4a8a5
Pinnapong <pinnapong@gmail.com>
2016-09-04 23:18:33 +0700

    Move deck tests into pile

--- test/deck.js
+++ test/deck.js
@@ -17,82 +17,9 @@ describe('Deck', function () {
         yield deck.load()
       })
 
-      it('create redis record of cards with the key', function* () {
-        const redisResult = yield client.getAsync('my-key:cards')
-        assert.notEqual(redisResult, null)
-      })
-
       it('initials 52 cards', function () {
         assert.lengthOf(deck.cards, 52)
       })
-    })
-
-    describe('existing key', function () {
-      let deck, existingDeck
-      beforeEach(function* () {
-        existingDeck = new Deck(client, 'my-key')
-        yield existingDeck.load()
-        yield existingDeck.draw()
-        deck = new Deck(client, 'my-key')
-        yield deck.load()
-      })
-
-      it('returns existing deck', function* () {
-        assert.lengthOf(deck.cards, 51)
-      })
-
-      it('reload cards from serialized data', function* () {
-        deck.cards.forEach((card) => {
-          assert.instanceOf(card, Card)
-        })
-      })
-    })
-
-  })
-
-  describe('#draw', function () {
-    let deck, card, topCard
-    beforeEach(function* () {
-      deck = new Deck(client, 'my-key')
-      yield deck.load()
-      topCard = deck.cards[deck.cards.length - 1]
-      card = yield deck.draw()
-    })
-
-    it('returns top card', function* () {
-      assert.deepEqual(card, topCard)
-    })
-
-    it('reduce remove top card from the deck', function* () {
-      assert.lengthOf(deck.cards, 51)
-    })
-
-    it('save changes to redis', function* () {
-      const newDeck = new Deck(client, 'my-key')
-      yield newDeck.load()
-      assert.lengthOf(newDeck.cards, 51)
-    })
-  })
-
-  describe('#shuffle', function () {
-    let deck, unshuffledDeck
-    beforeEach(function* () {
-      deck = new Deck(client, 'my-key')
-      yield deck.load()
-      unshuffledDeck = new Deck(client, 'another-key')
-      yield unshuffledDeck.load()
-      assert.deepEqual(deck.cards, unshuffledDeck.cards)
-      yield deck.shuffle()
-    })
-
-    it('change cards order', function* () {
-      assert.notDeepEqual(deck.cards, unshuffledDeck.cards)
-    })
-
-    it('save changes to redis', function* () {
-      const newDeck = new Deck(client, 'my-key')
-      yield newDeck.load()
-      assert.deepEqual(newDeck.cards, deck.cards)
     })
   })
 
---
+++ test/pile.js
@@ -0,0 +1,102 @@
+const Pile = require('../lib/pile')
+const Card = require('../lib/card')
+
+
+describe('Pile', function () {
+
+  beforeEach(function* () {
+    yield client.flushdbAsync()
+  })
+
+  describe('#load', function () {
+
+    describe('new key', function () {
+      let pile
+      beforeEach(function* () {
+        pile = new Pile(client, 'my-key')
+        yield pile.load()
+      })
+
+      it('create redis record of cards with the key', function* () {
+        const redisResult = yield client.getAsync('my-key:cards')
+        assert.notEqual(redisResult, null)
+      })
+
+      it('initials 0 cards', function () {
+        assert.lengthOf(pile.cards, 0)
+      })
+    })
+
+    describe('existing key', function () {
+      let pile, existingPile
+      beforeEach(function* () {
+        existingPile = new Pile(client, 'my-key')
+        yield existingPile.load()
+        existingPile.cards.push(new Card('2C'))
+        yield existingPile.save()
+        pile = new Pile(client, 'my-key')
+        yield pile.load()
+      })
+
+      it('returns existing pile', function* () {
+        assert.lengthOf(pile.cards, 1)
+      })
+
+      it('reload cards from serialized data', function* () {
+        pile.cards.forEach((card) => {
+          assert.instanceOf(card, Card)
+        })
+      })
+    })
+
+  })
+
+  describe('#draw', function () {
+    let pile, card
+    beforeEach(function* () {
+      pile = new Pile(client, 'my-key')
+      yield pile.load()
+      pile.cards.push(new Card('2C'))
+      pile.cards.push(new Card('3C'))
+      pile.cards.push(new Card('4C'))
+      card = yield pile.draw()
+    })
+
+    it('returns top card', function* () {
+      assert.deepEqual(card, new Card('4C'))
+    })
+
+    it('reduce remove top card from the pile', function* () {
+      assert.lengthOf(pile.cards, 2)
+    })
+
+    it('save changes to redis', function* () {
+      const newPile = new Pile(client, 'my-key')
+      yield newPile.load()
+      assert.lengthOf(newPile.cards, 2)
+    })
+  })
+
+  describe('#shuffle', function () {
+    let pile, cards = [new Card('2C'), new Card('3C'), new Card('4C')]
+    beforeEach(function* () {
+      pile = new Pile(client, 'my-key')
+      yield pile.load()
+      cards.forEach(c => pile.cards.push(c))
+      yield pile.save()
+      assert.deepEqual(pile.cards, cards)
+      yield pile.shuffle()
+    })
+
+    it('change cards order', function* () {
+      assert.notDeepEqual(pile.cards, cards)
+    })
+
+    it('save changes to redis', function* () {
+      const newPile = new Pile(client, 'my-key')
+      yield newPile.load()
+      assert.deepEqual(newPile.cards, pile.cards)
+    })
+  })
+
+})
